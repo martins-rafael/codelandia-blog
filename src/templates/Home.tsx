54f86f07ffaf06b855f56f444c7a8d8caf917944
Rafael Martins <rafaeldcmartins@gmail.com>
2021-08-25 15:38:10 -0300

    NewsCard component

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { Header } from 'components/Header';
+import { NewsCard } from 'components/NewsCard';
 
 export const HomeTemplate = () => (
   <div className="flex flex-col items-center justify-center min-h-screen bg-[#F3F5F7]">
@@ -10,8 +11,11 @@ export const HomeTemplate = () => (
 
     <Header />
 
-    <main className="flex flex-col items-center w-full flex-1 py-24 text-center ">
-
+    <main className="flex flex-col items-center w-full flex-1 py-24 px-5">
+      <NewsCard />
+      <NewsCard />
+      <NewsCard />
+      <NewsCard />
     </main>
   </div>
 );
