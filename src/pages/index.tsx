bb8bea1eaff685e6c341e7d4df83aba3fbda3b67
Rafael Martins <rafaeldcmartins@gmail.com>
2021-08-28 18:09:33 -0300

    Home static page generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,21 @@
+import client from 'graphql/client';
+import { GetStaticProps } from 'next';
+import { PostData } from 'types/post';
+import { GET_POSTS } from 'graphql/queries';
+import { GetPostsQuery } from 'graphql/generated/graphql';
 import { HomeTemplate } from "templates/Home";
 
-const Home = () => <HomeTemplate />;
+type HomeProps = {
+  posts: PostData[];
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { posts } = await client.request<GetPostsQuery>(GET_POSTS);
+
+  return {
+    props: { posts }
+  };
+};
+
+const Home = ({ posts }: HomeProps) => <HomeTemplate posts={posts} />;
 export default Home;
